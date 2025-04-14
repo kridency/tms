e88379ea57a9a35300086bc4e000cf4e2ad8b86d
sergei_fatykov <sergey.fatykov@gmail.com>
2025-04-14 19:02:06 +0300

    Add javadoc for user service.

--- src/main/java/com/example/taskmanagementsystem/securities/UserService.java
+++ src/main/java/com/example/taskmanagementsystem/securities/UserService.java
@@ -21,6 +21,13 @@ public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
+    /**
+     * Запускает обращение к базе данных пользователей для создания новой записи.
+     * Основной метод для создания записи пользователя в базе данных.
+     * @param request   объект описания аттрибутов создаваемой учетной записи пользователя
+     *
+     * @return  объект описания результата обращения к базе данных пользователей
+     */
     public UserDto create(AuthRequest request) {
         String email = request.getEmail();
         userRepository.getByEmail(email).ifPresent(user -> { throw new EntityExistsException("Email = " + email
@@ -28,6 +35,14 @@ public class UserService implements UserDetailsService {
         return userMapper.userToUserDto(userRepository.save(userMapper.authRequestToUser(request)));
     }
 
+    /**
+     * Запускает обращение к базе данных пользователей для обновления существующей записи.
+     * Основной метод для обновления записи пользователя в базе данных.
+     * @param request   объект описания аттрибутов создаваемой учетной записи пользователя
+     * @param username  адрес электронной почты пользователя, отправившего запрос на обновление учетной записи
+     *
+     * @return  объект описания результата обращения к базе данных пользователей
+     */
     public UserDto update(AuthRequest request, String username) {
         User user = findByEmail(username);
         String email = request.getEmail();
@@ -39,10 +54,24 @@ public class UserService implements UserDetailsService {
         return userMapper.userToUserDto(userRepository.save(user));
     }
 
+    /**
+     * Запускает обращение к базе данных пользователей для удаления существующей записи.
+     * Основной метод для удаления записи пользователя в базе данных.
+     * @param username  адрес электронной почты пользователя, отправившего запрос на удаление учетной записи
+     *
+     * @return  объект описания результата обращения к базе данных пользователей
+     */
     public UserDto delete(String username) {
         return userRepository.deleteByEmail(username).map(userMapper::userToUserDto).orElse(null);
     }
 
+    /**
+     * Преобразует объект отображения записи базы данных в объект используемый spring security.
+     * Перегрузка метода для получения объекта spring security.
+     * @param username  адрес электронной почты пользователя, отправившего запрос на аутентификацию
+     *
+     * @return  объект описания пользователя используемый spring security
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.getByEmail(username)
@@ -50,6 +79,13 @@ public class UserService implements UserDetailsService {
         return new AppUserDetails(user);
     }
 
+    /**
+     * Запускает обращение к базе данных пользователей для получения объекта записи согласно указанному адресу электронной почты.
+     * Вспомогательный метод для получения записи пользователя из базы данных.
+     * @param email  адрес электронной почты искомого пользователя
+     *
+     * @return  объект отображения записи базы данных пользователей
+     */
     public User findByEmail(String email) {
         return userRepository.getByEmail(email)
                 .orElseThrow(() -> new NotFoundException("Пользователь с Email = " + email + " не найден."));
